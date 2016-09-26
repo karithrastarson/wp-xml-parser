16dc542f3fdd89c23b150a32bde0c0ffb96292d4
Kári <karithrastarson@gmail.com>
2016-09-26 15:27:51 +0200

    cleanup

--- src/WPArchive.java
+++ src/WPArchive.java
@@ -1,13 +1,11 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
+import java.io.File;
+import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.util.ArrayList;
+
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
 
 /*
  * Created by kari.thrastarson on 22-09-2016.
